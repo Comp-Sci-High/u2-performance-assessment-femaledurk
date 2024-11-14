1af112139a8334d8cd25c5807d5b0048fbfd8936
femaledurk <chelseataylor.moore25@compscihigh.org>
2024-11-14 20:12:45 +0000

    finished post and get

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
+//GET
 async function cats (){
-const response  = await fetch ("https://meowfacts.herokuapp.com/")
-const data = await response.json()
-console.log (data.data[0])
-return data
+    const response  = await fetch ("https://meowfacts.herokuapp.com/")
+    const data = await response.json()
+    console.log (data.data[0])
+    return data
 }
 cats()
+
+//POST
+async function catImage(catData){
+const options = {
+    method: "POST",
+    headers: {
+        Authorization: "Bearer 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",
+        "Content-Type": "application/json",
+    },
+    body: JSON.stringify(catData),
+}
+const response = await fetch ("https://api.openai.com/v1/images/generations", options);
+const data = await response.json();
+console.log(data.data[0].url)
+}
+
+catImage({
+    prompt:"orange cat"
+})
+
+ 
+
+
+

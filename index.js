ce241298a185bb92f35e1579aea73ea4d635a030
femaledurk <chelseataylor.moore25@compscihigh.org>
2024-11-18 21:43:50 +0000

    finished

--- index.js
+++ index.js
@@ -1,32 +1,58 @@
 //GET
+
+const prompt = require("prompt-sync")()
+
 async function cats (){
+   try{
     const response  = await fetch ("https://meowfacts.herokuapp.com/")
+    if (response === false){
+        console.log ("HTTP error! Status: " + response.status);
+    }
     const data = await response.json()
     console.log (data.data[0])
     return data
+   } catch (error){
+    console.log("an error occured:" + error.message)
+   }
 }
-cats()
 
 //POST
 async function catImage(catData){
-const options = {
-    method: "POST",
-    headers: {
-        Authorization: "Bearer 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",
-        "Content-Type": "application/json",
-    },
-    body: JSON.stringify(catData),
+    try{
+    const options = {
+        method: "POST",
+        headers: {
+            Authorization: "Bearer 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",
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(catData),
+    }
+    const response = await fetch ("https://api.openai.com/v1/images/generations", options);
+    if (response === false){
+        console.log ("HTTP error! Status: " + response.status);
+    }
+    const data = await response.json();
+    console.log(data.data[0].url)
+}catch (error) {
+    console.log ("an error occured:" + error.message)
 }
-const response = await fetch ("https://api.openai.com/v1/images/generations", options);
-const data = await response.json();
-console.log(data.data[0].url)
 }
 
-catImage({
-    prompt:"orange cat"
-})
 
- 
+async function main(){
+
+  const fact = await cats()
+
+    let q1 = prompt("What cat would you like to see?")
+
+    let sentence = "Based on this random cat fact: " + fact + " and my description of a cat: " + q1 + " generate a photo"
+    
+    const image = await catImage({
+        prompt: sentence
+    })
+}
+
+main()
 
 
 
